63fb0d98998b0739e2a867cf0e53c8ee60fcc909
bon <bonujumanova@gmail.com>
2025-08-26 16:05:58 +0700

    Пользователь может выбрать несколько картинок для скачивания

--- wallpapers/parser.py
+++ wallpapers/parser.py
@@ -2,6 +2,7 @@ import time
 
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 from pprint import pprint
 
 from wallpapers.constants import CATEGORIES_MAP, SITE_URL
@@ -22,7 +23,7 @@ def show_categories(categories: dict[str, str]) -> None:
 def show_images(data: dict[str, list]) -> None:
     length: int = int(len(str(len(data))))
     for number, value in enumerate(data.values(), 1):
-        print(f"{number:>{length}}. {' '.join(item for item in value)}")
+        print(f"{number:>{length}}. {' '.join(value)}")
     # TODO: Реализовать вывод пронумерованных картинок:
     #  - Название
     #  - Ключевые слова
@@ -30,19 +31,54 @@ def show_images(data: dict[str, list]) -> None:
     pass
 
 
-def get_image(images: dict[str, list], numbers: list[int]):
+def get_image(images: dict[str, list], numbers: list[int], url: str):
     # Тут все таки нужно возвращать словарь, name: link, тогда в дальнейшем будет удобнее работать, точнее распаовывать/
     # Хотя с другой стороны список с кортежами(name, link) тоже не сложно распаковать. Во втором случае придется/
     # прибегнуть к циклу
 
-    selected_images_name_link: list[tuple] = []
+    selected_images_name_link: dict = {}
     for number in numbers:
-        print(number)
+
         image_link = list(images.keys())[number - 1]
         image_name = images.get(image_link)
-        selected_images_name_link.append((image_name, image_link))
+        selected_images_name_link[url + image_link] = image_name
 
     return selected_images_name_link
+
+
+def get_image_resolution(image_link: str):
+    print(image_link)
+    response = requests.get(image_link)
+    soup = BeautifulSoup(response.text, features="html.parser")
+    widescreen_resolutions = soup.find_all("div", class_="resolutions__title gui-h3")
+    print(widescreen_resolutions)
+    # Не понимаю как же найти ссылку на конкретное разрешение..... В разрешениях для телефона, apple /
+    # используются одинаковые названия+
+    for li in widescreen_resolutions:
+        # Вот тут ниже в поиске - ошибка, тк в print(f"a.contents === {a.contents}") выводятся только
+        # первые элементы подзаголовков. Нужно искать по другому
+        a = (li.find_next("div",
+                         class_="resolutions__cell resolutions__caption").find_next
+             ("a", class_="resolutions__link"))
+        if a:
+            print(f"a.contents === {a.contents}")
+            if a.contents == '1920x1080' or a.contents == ["1920x1080"]:
+                link: str = a.get("href")
+                return link
+    return "Такого разрешения нет"
+
+# def download_image(img_link, url):
+#     response = requests.get(url + img_link)
+#     soup = BeautifulSoup(response.text, features="html.parser")
+#
+#     a = img_link.find_all("a", class_="gui-button gui-button_full-height")
+#     finally_download_link: str = a.get("href")
+#     print(f"download_image: {finally_download_link}")
+#
+#     if finally_download_link:
+#         response = requests.get(finally_download_link)
+#         with open("images/ssss.png", "ab+") as f:
+#             f.write(response.content)
 
 
 
@@ -122,10 +158,10 @@ def main():
                                                 choice_category_number)
 
     print(f"Selected category '{category_name}' - {category_link}")
-
+    print("Loading...")
     all_images_names_and_links: dict[str, list] = {}
 
-    for page_num in range(1, 5):
+    for page_num in range(1, 2):
         curr_page: str = get_category_page(category_link, page_num)
         images_on_page: dict[str, list] = parse_images_on_page(curr_page, all_images_names_and_links)
         time.sleep(10)
@@ -140,11 +176,23 @@ def main():
         if choice_image_number == 'q':
             break
         elif not (1 <= int(choice_image_number) <= images_length):
-            raise ValueError("Unavailable image")
+            break
         else:
             selected_images_nums.append(int(choice_image_number))
+    url: str = SITE_URL[choice_lang]
+    images_names_and_links = get_image(all_images_names_and_links, selected_images_nums, url)
 
-    image_name = get_image(all_images_names_and_links, selected_images_nums)
+    for image_link, image_name in images_names_and_links.items():
+
+        img_lnk_for_dwnldng = get_image_resolution(image_link)
+        print("img_lnk_for_dwnldng",img_lnk_for_dwnldng)
+        # if img_lnk_for_dwnldng:
+        #
+        #     download_image(img_lnk_for_dwnldng, url)
+        #
+
+
+
     # В этом случае важно обратить внимание на то, что возвращает get_image()
     # if choice_lang == 'en':
     #        image_link = SITE_URL['en'] + image_link
